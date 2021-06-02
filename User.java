484c99f5bc9952be8cb13a847ac48a679c99152e
kristinakorda <kristina.korda@gmail.com>
2021-06-02 16:46:58 +0200

    metode za login i registraciju
    Co-Authored-By: peradjura <84920838+peradjura@users.noreply.github.com>

---
+++ User.java
@@ -0,0 +1,59 @@
+package pozoriste.user;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4047951643938524972L;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String type = "user"; // admin or user
+
+	public User() {
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
