a0e93ad800b58da65792898e132befc4d3f40e4e
vanjalovrencic <lovrev10@gmail.com>
2021-06-03 12:21:40 +0200

    Sacuvavanje

--- PozoristeProjekat/src/pozoriste/Main.java
+++ PozoristeProjekat/src/pozoriste/Main.java
@@ -1,9 +1,23 @@
 package pozoriste;
 
+import java.io.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		new MainWindow();
+	}
+
+	public static void saveToFile(Object obj, String file) {
+		try {
+			FileOutputStream f = new FileOutputStream(new File(file));
+			ObjectOutputStream o = new ObjectOutputStream(f);
+			o.writeObject(obj);
+			o.close();
+			f.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
 	}
 
 }
