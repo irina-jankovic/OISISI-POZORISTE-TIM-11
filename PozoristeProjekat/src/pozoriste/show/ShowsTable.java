c0dfcd4ea185fc652430c0d1f0c96aa51c77a66a
irina-jankovic <irina.jankovic9@gmail.com>
2021-06-14 16:30:00 +0200

    Update ShowsTable.java

--- PozoristeProjekat/src/pozoriste/show/ShowsTable.java
+++ PozoristeProjekat/src/pozoriste/show/ShowsTable.java
@@ -1,11 +1,13 @@
 package pozoriste.show;
 
-import java.awt.Font;
+import pozoriste.MainWindow;
+import pozoriste.user.UserFunctions;
 
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 public class ShowsTable extends JPanel {
 
@@ -15,6 +17,40 @@ public class ShowsTable extends JPanel {
 	private static final long serialVersionUID = 4455631963210013570L;
 
 	public ShowsTable() {
+		setLayout(new BorderLayout());
+		Color backgroundColor = new Color(240, 240, 240);
+
+		JButton logout = new JButton("Odjava");
+		logout.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		logout.setBackground(backgroundColor);
+
+		JPanel lp = new JPanel();
+		logout.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				UserFunctions.logout();
+			}
+		});
+		lp.add(logout);
+
+		if (UserFunctions.getCurrentUser().getType().equals("admin")) {
+			JButton addnew = new JButton("Dodaj");
+			addnew.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			addnew.setBackground(backgroundColor);
+			addnew.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					MainWindow.setView(new NewShowPage(null));
+
+				}
+			});
+			lp.add(addnew);
+		}
+
+		add(lp, BorderLayout.NORTH);
+
 		JTable table = new JTable();
 		table.setRowHeight(22);
 		table.setRowSelectionAllowed(true);
@@ -23,6 +59,29 @@ public class ShowsTable extends JPanel {
 		table.setModel(new TableModelForShows());
 		table.getTableHeader().setFont(new Font("arial", Font.BOLD, 14));
 		add(new JScrollPane(table));
+
+		table.setAutoCreateRowSorter(true);
+
+		if (UserFunctions.getCurrentUser().getType().equals("admin")) {
+
+			Action edit = new AbstractAction() {
+				/**
+				 *
+				 */
+				private static final long serialVersionUID = 1585299714856884984L;
+
+				public void actionPerformed(ActionEvent e) {
+					int modelRow = Integer.valueOf(e.getActionCommand());
+					MainWindow.setView(new NewShowPage(ShowFunctions.getShows().get(modelRow)));
+
+				}
+			};
+
+			new ButtonColumn(table, edit, 5);
+		}
+
+		// more details
+	
 	}
 
 }
